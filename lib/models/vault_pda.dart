02796fb0dafdd9fa27231973b30b990a34634a10
Arj-Lab <arj.lemited@gmail.com>
2025-11-07 23:45:05 +0300

    fix: added vault parameter

--- lib/models/vault_pda.dart
+++ lib/models/vault_pda.dart
@@ -8,13 +8,14 @@ class VaultPda {
   final int baseFee;
   final String mint;
   final String pythPriceFeedAccount;
+  final int maxAgePrice;
   final String lpTokenMint;
   final int initialLiquidity;
   final int currentLiquidity;
   final int cumulativeYield;
   final int protocolYield;
 
-  VaultPda({required this.address, required this.isActive, required this.baseFee, required this.mint, required this.pythPriceFeedAccount, required this.lpTokenMint, required this.initialLiquidity, required this.cumulativeYield, required this.currentLiquidity, required this.protocolYield});
+  VaultPda({required this.address, required this.isActive, required this.baseFee, required this.mint, required this.pythPriceFeedAccount, required this.maxAgePrice, required this.lpTokenMint, required this.initialLiquidity, required this.cumulativeYield, required this.currentLiquidity, required this.protocolYield});
 
   factory VaultPda.fromProgramAccount(ProgramAccount programAccount) {
     var binaryAccountData = programAccount.account.data as BinaryAccountData;
@@ -25,11 +26,12 @@ class VaultPda {
       baseFee: Int64.fromBytes(data.getRange(9, 17).toList()).toInt(),
       mint: base58encode(data.getRange(17, 49).toList()), 
       pythPriceFeedAccount: base58encode(data.getRange(49, 81).toList()), 
-      lpTokenMint: base58encode(data.getRange(81, 113).toList()), 
-      initialLiquidity: Int64.fromBytes(data.getRange(113, 121).toList()).toInt(), 
-      currentLiquidity: Int64.fromBytes(data.getRange(121, 129).toList()).toInt(),
-      cumulativeYield: Int64.fromBytes(data.getRange(129, 145).toList()).toInt(),
-      protocolYield: Int64.fromBytes(data.getRange(145, 153).toList()).toInt()
+      maxAgePrice: Int64.fromBytes(data.getRange(81, 89).toList()).toInt(),
+      lpTokenMint: base58encode(data.getRange(89, 121).toList()), 
+      initialLiquidity: Int64.fromBytes(data.getRange(121, 129).toList()).toInt(), 
+      currentLiquidity: Int64.fromBytes(data.getRange(129, 137).toList()).toInt(),
+      cumulativeYield: Int64.fromBytes(data.getRange(137, 153).toList()).toInt(),
+      protocolYield: Int64.fromBytes(data.getRange(153, 161).toList()).toInt()
     );
   }
 }
