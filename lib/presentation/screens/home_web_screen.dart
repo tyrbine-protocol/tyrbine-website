3556e03b8390662973b6990fd57b3a3c22fcfb7c
Arj-Lab <arj.lemited@gmail.com>
2025-11-10 12:49:29 +0300

    feat: added stat screen

--- lib/presentation/screens/home_web_screen.dart
+++ lib/presentation/screens/home_web_screen.dart
@@ -15,6 +15,7 @@ import 'package:tyrbine_website/utils/extensions.dart';
 import 'package:tyrbine_website/widgets/stars_progress_indicator.dart';
 import 'package:tyrbine_website/widgets/custom_inkwell.dart';
 import 'package:tyrbine_website/widgets/stakes_list.dart';
+import 'package:tyrbine_website/widgets/hover_container_card.dart';
 
 class HomeWebScreen extends ConsumerStatefulWidget {
   final String vaultMint;
@@ -130,93 +131,7 @@ class _HomeWebScreenState extends ConsumerState<HomeWebScreen>
                           mainAxisAlignment: MainAxisAlignment.center,
                           crossAxisAlignment: CrossAxisAlignment.center,
                           children: [
-                            Container(
-                              width: 400.0,
-                              padding: const EdgeInsets.all(16.0),
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(10.0),
-                                  border: Border.all(
-                                      color: Colors.grey.shade600, width: 0.15)),
-                              child: Column(
-                                mainAxisAlignment: MainAxisAlignment.center,
-                                crossAxisAlignment: CrossAxisAlignment.center,
-                                children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                    crossAxisAlignment: CrossAxisAlignment.center,
-                                    children: [
-                                      Column(
-                                        mainAxisAlignment: MainAxisAlignment.start,
-                                        crossAxisAlignment: CrossAxisAlignment.start,
-                                        children: [
-                                          const Text('Total Value Locked',
-                                              style: TextStyle(
-                                                  color: Color(0xFF5F5B5B))),
-                                          Text('\$${stat.totalTvl.formatNumWithCommas()}',
-                                              style:
-                                                  const TextStyle(fontSize: 26.0)),
-                                          Padding(
-                                    padding: const EdgeInsets.only(top: 8.0),
-                                    child: Row(
-                                      mainAxisSize: MainAxisSize.min,
-                                      children: [
-                                        Image.asset(
-                                            stat.dailyChangeTvlAmount >= 0
-                                                ? 'assets/icons/arrow_up.png'
-                                                : 'assets/icons/arrow_down.png',
-                                            height: 15.0,
-                                            width: 15.0),
-                                        const SizedBox(width: 8.0),
-                                        Text('${stat.dailyChangeTvlPercent}%',
-                                            style: const TextStyle(
-                                                color: Color(0xFF5F5B5B),
-                                                fontSize: 14.0)),
-                                        const SizedBox(width: 8.0),
-                                        const Text('·',
-                                            style: TextStyle(
-                                                color: Color(0xFF5F5B5B),
-                                                fontSize: 14.0)),
-                                        const SizedBox(width: 8.0),
-                                        Text(
-                                            stat.dailyChangeTvlAmount >= 0
-                                                        ? '+\$${stat.dailyChangeTvlAmount.formatNumWithCommas()}'
-                                                        : '-\$${stat.dailyChangeTvlAmount.abs().formatNumWithCommas()}',
-                                                    style: TextStyle(
-                                                        color:
-                                                            stat.dailyChangeTvlAmount >= 0
-                                                                ? Colors.greenAccent
-                                                                : Colors.red,
-                                                        fontSize: 14.0)),
-                                                const SizedBox(width: 8.0),
-                                                const Text('last 24h',
-                                                    style: TextStyle(
-                                                        color: Color(0xFF5F5B5B),
-                                                        fontSize: 14.0)),
-                                              ],
-                                            ),
-                                          )
-                                        ],
-                                      ),
-                                      Container(
-                                        height: 40.0,
-                                        width: 40.0,
-                                        alignment: Alignment.center,
-                                        decoration: const BoxDecoration(
-                                            gradient: LinearGradient(colors: [
-                                              Color(0xFF1C1C1C),
-                                              Color(0xFF121212)
-                                            ]),
-                                            shape: BoxShape.circle),
-                                        child: Image.asset(
-                                            'assets/icons/lock_icon.png',
-                                            height: 18.0,
-                                            width: 18.0),
-                                      )
-                                    ],
-                                  ),
-                                ],
-                              ),
-                            ),
+                            HoverContainerCard(stat: stat),
                             const SizedBox(height: 16.0),
                             SvgPicture.asset("assets/icons/stars.svg",
                                 height: 12.0),
